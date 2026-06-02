65d88348182b632dade0236e0d172e038e0eeaf5
jlbuild <juliabuildbot@gmail.com>
2026-06-02 17:51:57 +0000

    GAP_pkg_cvec_jll build 201.80500.0+0

--- src/GAP_pkg_cvec_jll.jl
+++ src/GAP_pkg_cvec_jll.jl
@@ -5,5 +5,5 @@ using Base: UUID
 import JLLWrappers
 
 JLLWrappers.@generate_main_file_header("GAP_pkg_cvec")
-JLLWrappers.@generate_main_file("GAP_pkg_cvec", UUID("03de9d45-54dd-5e36-9810-6ca1d577e6b3"))
+JLLWrappers.@generate_main_file("GAP_pkg_cvec", Base.UUID("03de9d45-54dd-5e36-9810-6ca1d577e6b3"))
 end  # module GAP_pkg_cvec_jll
